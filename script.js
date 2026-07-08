80f740994e51dcd4d3f8d3e7d299798fe759b3ea
Farhan0534T <farhan0534t@gmail.com>
2026-07-08 21:27:43 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,74 @@
-function addAnime(){
+// Firebase
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-app.js";
+import {
+  getDatabase,
+  ref,
+  push,
+  onValue,
+  remove
+} from "https://www.gstatic.com/firebasejs/12.15.0/firebase-database.js";
 
-    let input=document.getElementById("animeInput");
-    let anime=input.value.trim();
+// Firebase Config
+const firebaseConfig = {
+  apiKey: "AIzaSyDH8Na-I0CPJoN-ElAi2sHfUt3Yv5iKSMg",
+  authDomain: "legendary-anime-watchlist.firebaseapp.com",
+  databaseURL: "https://legendary-anime-watchlist-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "legendary-anime-watchlist",
+  storageBucket: "legendary-anime-watchlist.firebasestorage.app",
+  messagingSenderId: "964225790114",
+  appId: "1:964225790114:web:c2752650f9ac3b61e567fb"
+};
 
-    if(anime===""){
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getDatabase(app);
+
+// HTML Elements
+const input = document.getElementById("animeInput");
+const list = document.getElementById("animeList");
+const addBtn = document.getElementById("addBtn");
+
+// Add Anime
+addBtn.addEventListener("click", () => {
+
+    const anime = input.value.trim();
+
+    if (anime === "") {
         alert("Enter an anime name!");
         return;
     }
 
-    let li=document.createElement("li");
+    push(ref(db, "watchlist"), {
+        name: anime
+    });
 
-    li.innerHTML=`
-    <span>${anime}</span>
-    <button class="delete">Delete</button>
-    `;
+    input.value = "";
 
-    li.querySelector("button").onclick=function(){
-        li.remove();
-    }
+});
 
-    document.getElementById("animeList").appendChild(li);
+// Show Anime List
+onValue(ref(db, "watchlist"), (snapshot) => {
 
-    input.value="";
-}
+    list.innerHTML = "";
+
+    snapshot.forEach((child) => {
+
+        const data = child.val();
+        const key = child.key;
+
+        const li = document.createElement("li");
+
+        li.innerHTML = `
+            <span>${data.name}</span>
+            <button class="delete">Delete</button>
+        `;
+
+        li.querySelector(".delete").addEventListener("click", () => {
+            remove(ref(db, "watchlist/" + key));
+        });
+
+        list.appendChild(li);
+
+    });
+
+});
