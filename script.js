b1e1965a3957b4e847cd84b5b21368995c47f775
Farhan0534T <farhan0534t@gmail.com>
2026-07-09 10:10:13 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,132 +1,65 @@
-alert("Script Loaded");
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-app.js";
+import {
+  getDatabase,
+  ref,
+  push,
+  onValue
+} from "https://www.gstatic.com/firebasejs/12.15.0/firebase-database.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDH8Na-I0CPJoN-ElAi2sHfUt3Yv5iKSMg",
+  authDomain: "legendary-anime-watchlist.firebaseapp.com",
+  databaseURL: "https://legendary-anime-watchlist-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "legendary-anime-watchlist",
+  storageBucket: "legendary-anime-watchlist.firebasestorage.app",
+  messagingSenderId: "964225790114",
+  appId: "1:964225790114:web:c2752650f9ac3b61e567fb"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getDatabase(app);
+
 const animeInput = document.getElementById("animeInput");
 const addBtn = document.getElementById("addBtn");
 const animeList = document.getElementById("animeList");
-const searchInput = document.getElementById("searchInput");
 
-let animeData = [];
+addBtn.addEventListener("click", () => {
 
-// Add Anime
-addBtn.addEventListener("click", addAnime);
-
-function addAnime(){
-
-    let name = animeInput.value.trim();
+    const name = animeInput.value.trim();
 
     if(name === ""){
         alert("Enter anime name!");
         return;
     }
 
-    let anime = {
-        id: Date.now(),
-        name: name,
-        rating: "N/A",
-        status: "Plan to Watch"
-    };
-
-    animeData.push(anime);
-
-    showAnime(animeData);
-
-    animeInput.value = "";
-
-}
-
-
-// Show Anime
-function showAnime(data){
-
-    animeList.innerHTML = "";
-
-    data.forEach(anime => {
-
-        let card = document.createElement("div");
-
-        card.className = "card";
-
-        card.innerHTML = `
-
-        <div>
-            <h3>🎌 ${anime.name}</h3>
-            <p>⭐ Rating: ${anime.rating}</p>
-            <p>📺 Status: ${anime.status}</p>
-        </div>
-
-        <div class="actions">
-
-            <button class="editBtn">
-                Edit
-            </button>
-
-            <button class="deleteBtn">
-                Delete
-            </button>
-
-        </div>
-
-        `;
-
-
-        // Delete
-        card.querySelector(".deleteBtn").onclick = function(){
-
-            animeData = animeData.filter(
-                item => item.id !== anime.id
-            );
-
-            showAnime(animeData);
-
-        };
-
-
-        // Edit
-        card.querySelector(".editBtn").onclick = function(){
-
-            let newName = prompt(
-                "Edit anime name:",
-                anime.name
-            );
-
-            if(newName){
-
-                anime.name = newName;
-
-                showAnime(animeData);
-
-            }
-
-        };
-
-
-        animeList.appendChild(card);
-
+    push(ref(db,"watchlist"),{
+        name:name,
+        rating:"N/A",
+        status:"Plan to Watch"
     });
 
-}
-
-
-// Search
-searchInput.addEventListener("input", function(){
-
-    let value = searchInput.value.toLowerCase();
-
-
-    let filtered = animeData.filter(anime =>
-        anime.name.toLowerCase().includes(value)
-    );
-
-
-    showAnime(filtered);
+    animeInput.value="";
 
 });
 
+onValue(ref(db,"watchlist"),(snapshot)=>{
 
-// Enter key add
-animeInput.addEventListener("keypress", function(e){
+    animeList.innerHTML="";
 
-    if(e.key === "Enter"){
-        addAnime();
-    }
+    snapshot.forEach((child)=>{
+
+        const anime=child.val();
+
+        animeList.innerHTML+=`
+        <div class="card">
+            <div>
+                <h3>🎌 ${anime.name}</h3>
+                <p>⭐ ${anime.rating}</p>
+                <p>📺 ${anime.status}</p>
+            </div>
+        </div>
+        `;
+
+    });
 
 });
