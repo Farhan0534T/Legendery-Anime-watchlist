dff429489f9e1cfd3f42b49a71cf3a48c6765a2f
Farhan0534T <farhan0534t@gmail.com>
2026-07-09 10:48:22 +0600

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,9 @@ import {
   getDatabase,
   ref,
   push,
-  onValue
+  onValue,
+  remove,
+  update
 } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-database.js";
 
 const firebaseConfig = {
@@ -33,10 +35,10 @@ addBtn.addEventListener("click", () => {
     }
 
     push(ref(db,"watchlist"),{
-        name:name,
-        rating:"N/A",
-        status:"Plan to Watch"
-    });
+    name: name,
+    rating: "0",
+    status: "Plan to Watch"
+});
 
     animeInput.value="";
 
@@ -50,16 +52,108 @@ onValue(ref(db,"watchlist"),(snapshot)=>{
 
         const anime=child.val();
 
-        animeList.innerHTML+=`
-        <div class="card">
-            <div>
-                <h3>🎌 ${anime.name}</h3>
-                <p>⭐ ${anime.rating}</p>
-                <p>📺 ${anime.status}</p>
-            </div>
-        </div>
-        `;
+       const key = child.key;
+
+animeList.innerHTML += `
+<div class="card">
+
+<div>
+
+<h3>🎌 ${anime.name}</h3>
+
+<p>
+Rating:
+<select class="rating" data-key="${key}" data-value="${anime.rating}">
+<option value="0">0</option>
+<option value="0.5">0.5</option>
+<option value="1">1</option>
+<option value="1.5">1.5</option>
+<option value="2">2</option>
+<option value="2.5">2.5</option>
+<option value="3">3</option>
+<option value="3.5">3.5</option>
+<option value="4">4</option>
+<option value="4.5">4.5</option>
+<option value="5">5</option>
+<option value="5.5">5.5</option>
+<option value="6">6</option>
+<option value="6.5">6.5</option>
+<option value="7">7</option>
+<option value="7.5">7.5</option>
+<option value="8">8</option>
+<option value="8.5">8.5</option>
+<option value="9">9</option>
+<option value="9.5">9.5</option>
+<option value="10">10</option>
+</select>
+</p>
+
+<p>
+
+Status:
+
+<select class="status" data-key="${key}" data-value="${anime.status}">
+
+<option>Plan to Watch</option>
+<option>Watching</option>
+<option>Completed</option>
+<option>On Hold</option>
+<option>Dropped</option>
+
+</select>
+
+</p>
+
+<button class="deleteBtn" data-key="${key}">
+Delete
+</button>
+
+</div>
+
+</div>
+`;
 
     });
+  // Delete
+document.querySelectorAll(".deleteBtn").forEach(btn => {
+
+    btn.onclick = () => {
+
+        remove(ref(db, "watchlist/" + btn.dataset.key));
+
+    };
+
+});
+document.querySelectorAll(".rating").forEach(select=>{
+
+    select.value = select.dataset.value;
+
+    select.onchange=()=>{
+
+        update(ref(db,"watchlist/"+select.dataset.key),{
+
+            rating:select.value
+
+        });
+
+    };
+
+});
+
+document.querySelectorAll(".status").forEach(select=>{
+
+    select.value = select.dataset.value;
+
+    select.onchange=()=>{
+
+        update(ref(db,"watchlist/"+select.dataset.key),{
+
+            status:select.value
+
+        });
+
+    };
+
+});
 
 });
